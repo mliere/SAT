99e3b948315bf9e33b22eb5ba80620b8bd5f82df
ml90864 <martinvanliere@outlook.com>
2020-11-07 21:30:33 +0100

    set up initial skeleton project.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SAT
 Shape Analysis Tool
+
+## requirements
+- Maven https://maven.apache.org/guides/getting-started/
+- docker
+
+### run the application
+'mvn clean javafx:run'
+this will compile application with dependencies and run.
---
+++ src/main/java/SAT/App.java
@@ -0,0 +1,59 @@
+package SAT;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+ 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+ 
+        // Buat Object Button
+        Button btn1 = new Button("Button One");
+ 
+        // Atur posisi Button berdasarkan baris dan kolom
+        GridPane.setRowIndex(btn1, 0);
+        GridPane.setColumnIndex(btn1, 0);
+ 
+        Button btn2 = new Button("Button Two");
+        GridPane.setRowIndex(btn2, 0);
+        GridPane.setColumnIndex(btn2, 1);
+ 
+        Button btn3 = new Button("Button Three");
+        GridPane.setRowIndex(btn3, 0);
+        GridPane.setColumnIndex(btn3, 2);
+ 
+        Button btn4 = new Button("Button Four");
+        GridPane.setRowIndex(btn4, 1);
+        GridPane.setColumnIndex(btn4, 0);
+ 
+        Button btn5 = new Button("Button Five");
+        GridPane.setRowIndex(btn5, 1);
+        GridPane.setColumnIndex(btn5, 1);
+ 
+        // Buat Object GridPane
+        GridPane gridpane = new GridPane();
+ 
+        // Atur posisi dan celah antar kolom GridPane
+        gridpane.setAlignment(Pos.CENTER);
+        gridpane.setHgap(10);
+        gridpane.setVgap(10);
+ 
+        gridpane.getChildren().addAll(btn1, btn2, btn3, btn4, btn5);
+ 
+        Scene scene = new Scene(gridpane, 900, 500);
+ 
+        primaryStage.setTitle("Shape Analysis Tool");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ 
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
