ab5f42b9c62eaf2e9d68cb0a2375e89ded115c4d
Johan de Lijser <johan@lyfter.nl>
2020-12-08 21:16:39 +0100

    First development for mainview class

--- src/main/java/SAT/App.java
+++ src/main/java/SAT/App.java
@@ -1,75 +1,20 @@
 package SAT;
 
 import SAT.data.MySqlConnection;
-import SAT.data.Xml;
-import SAT.model.shape.Cube;
+import SAT.view.MainView;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-  @Override
-  public void start(final Stage primaryStage) throws Exception{
+    public MySqlConnection connection;
 
-    MySqlConnection connection = new MySqlConnection();
+    public void start(final Stage primaryStage) throws Exception {
+        connection = new MySqlConnection();
+        MainView mainView = new MainView(primaryStage);
+    }
 
-    //Button btn1 = new Button(connection.getData());
-    Button btn1 = new Button("1");
-    //Creating the mouse event handler
-    EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent e) {
-        GridPane grid = new GridPane();
-        primaryStage.getScene().setRoot(grid);
-      }
-    };
-    //Registering the event filter
-    btn1.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+    public static void main(String[] args) {
 
-    GridPane.setRowIndex(btn1, 0);
-    GridPane.setColumnIndex(btn1, 0);
-
-    Button btn2 = new Button("Button Two");
-    GridPane.setRowIndex(btn2, 0);
-    GridPane.setColumnIndex(btn2, 1);
-
-    Button btn3 = new Button("Button Three");
-    GridPane.setRowIndex(btn3, 0);
-    GridPane.setColumnIndex(btn3, 2);
-
-    Button btn4 = new Button("Button Four");
-    GridPane.setRowIndex(btn4, 1);
-    GridPane.setColumnIndex(btn4, 0);
-
-    Button btn5 = new Button("Button Five");
-    GridPane.setRowIndex(btn5, 1);
-    GridPane.setColumnIndex(btn5, 1);
-
-    // Buat Object GridPane
-    GridPane gridpane = new GridPane();
-
-    // Atur posisi dan celah antar kolom GridPane
-    gridpane.setAlignment(Pos.CENTER);
-    gridpane.setHgap(10);
-    gridpane.setVgap(10);
-
-    gridpane.getChildren().addAll(btn1, btn2, btn3, btn4, btn5);
-
-    Scene scene = new Scene(gridpane, 900, 500);
-
-    primaryStage.setTitle("Shape Analysis Tool");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-
-  }
-
-  public static void main(String[] args) {
-    launch(args);
-  }
+    }
 }
--- src/main/java/SAT/view/MainView.java
+++ src/main/java/SAT/view/MainView.java
@@ -1,5 +1,58 @@
 package SAT.view;
 
-public class MainView {
+import SAT.App;
+import SAT.data.MySqlConnection;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class MainView extends App {
+
+    private GridPane gridPane;
+
+    public MainView(Stage primaryStage) {
+        gridPane = new GridPane();
+
+        createGridPane();
+        addCalculateButton();
+
+        Scene mainScene = new Scene(gridPane, 900, 500);
+
+        primaryStage.setTitle("Shape Analysis Tool");
+        primaryStage.setScene(mainScene);
+        System.out.println("HALLOOO");
+        primaryStage.show();
+    }
+
+    private void createGridPane()
+    {
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+    }
+
+    private void addCalculateButton() {
+        Button calculateButton = new Button("calculate");
+
+//        //Creating the mouse event handler
+//        EventHandler<MouseEvent> onCalculateClick = new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent e) {
+//                GridPane grid = new GridPane();
+//                mainStage.getScene().setRoot(grid);
+//            }
+//        };
+//
+//        //Registering the event filter
+//        calculateButton.addEventFilter(MouseEvent.MOUSE_CLICKED, onCalculateClick);
+
+        GridPane.setRowIndex(calculateButton, 0);
+        GridPane.setColumnIndex(calculateButton, 0);
+
+        gridPane.getChildren().addAll(calculateButton);
+    }
 }
