14189e5abb3285d98a6ab35f0db65e3664f6a844
ml90864 <martinvanliere@outlook.com>
2020-12-13 21:13:35 +0100

    xml file now produces an Shape object

--- src/main/java/SAT/data/Xml.java
+++ src/main/java/SAT/data/Xml.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.BufferedReader;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import SAT.model.shape.*;
+
+// used for writing xml
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
+// used for reading xml
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.InputSource;
 
 public class Xml{
 
@@ -68,9 +75,77 @@ public class Xml{
     return xmlString;
   }
 
-  public static Object fromXml(String xml) {
+  public static Shape fromXml(String xml) {
+    try {
 
-    System.out.println(xml);
-      return new Object();  
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+      SAXParser saxParser = factory.newSAXParser();
+
+      DefaultHandler handler = new DefaultHandler() {
+
+        String identifier;
+        String shapeType;
+        boolean bId=false;
+        boolean bAttr=false;
+
+        List<String> attributes = new ArrayList<String>();
+
+        public void startElement(String uri, String localName,String qName, 
+            Attributes attributes) throws SAXException {
+
+          System.out.println("Start Element :" + qName);
+          if(qName=="id") {bId = true;}
+
+          // assume only 1 shape per document
+          if(qName=="Cube") {shapeType = qName;}
+          System.out.println(shapeType);
+
+          if(qName=="length") {bAttr = true;}
+          if(qName=="width") {bAttr = true;}
+          if(qName=="height") {bAttr = true;}
+        }
+
+        public void endElement(String uri, String localName,
+            String qName) throws SAXException {
+
+          System.out.println("End Element :" + qName);
+          if(qName=="Document") {
+            String[] attributeArray = new String[attributes.size()];
+            attributeArray = attributes.toArray(attributeArray);
+            Object object = ShapeFactory.getShape(shapeType,attributeArray);
+
+          }
+
+        }
+
+        public void characters(char ch[], int start, int length) throws SAXException {
+
+          if (bId) {
+            identifier = new String(ch, start, length);
+            System.out.println("Unique identifier: " + identifier);
+            bId = false;
+          }
+
+          if (bAttr) {
+            attributes.add(new String(ch, start, length));
+            bAttr = false;
+          }
+
+        }
+
+      };
+
+      saxParser.parse(new InputSource(new StringReader(xml)), handler);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return null;
   }
 }
+
+
+
+
